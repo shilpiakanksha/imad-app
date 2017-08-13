9c3bdfa5f1b29391154059aa7a36b9fe5cefd2bd
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-13 20:58:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,101 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles= {
- 'article-One': {
- title: 'Article-one|Akanksha Dixit',
- heading: 'article One ',
- date: 'sep 5, 2016',
- content: `<p>
-             this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article
-            </p>
-            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
-          </p>
-            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
-            </p>`
-            
-    
-    
-    
-},
-'article-Two': {
- title: 'Article-Two|Akanksha Dixit',
- heading: 'Article Two',
- date: 'sep 5, 2016',
- content: `<p>
-             this is content of my first article</p>`,
-            
-    
-    
-    
-},
-  'article-Three': {
- title: 'Article-Three|Akanksha Dixit',
- heading: 'article Three',
- date: 'sep 5, 2016',
- content: `<p>
-             this is content of my first article</p>`,
-            
-    
-}
-};
-
-function createTemplate(data){
- var title=data.title;
- var date=data.date;
- var heading=data.heading;
- var content=data.content;
-
-var htmltemplate= `<html>
-    <head>
-    <title>
-    ${title}
-    </title>
-     
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                   <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-
-    <body>
-        <div class="container">
-       <div>
-           <a href="/">Home</a>
-           
-        </div>  
-        <hr/>
-        ${heading}
-        <div>
-            ${date}
-        </div>    
-        <div>
-            ${content}
-        </div>        
- 
-          </div>
-           
-    </body>    
-    
-</html>
-`; 
-return createTemplate;
-} 
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function (req,res){
-    //articleName==article-One
-    //articles[articleName]=={}content object for article one
-    var articleName=req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
-});
-app.get('/article-two',function (req,res){
-     res.send(createTemplate(articleTwo));
-});
-app.get('/article-three',function (req,res){
-      res.send(createTemplate(articleThree));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
