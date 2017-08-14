5a272806278b587f1a4749b7842b1b856cc04722
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-14 15:00:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,1 +1,104 @@
-console.log('Loaded!');
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+var articles={
+     'article-one': {
+    
+    title:'article-one|Akanksha Dixit',
+    heading:'article-one',
+    date:'sep 05,2016',
+    content:`<p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article
+            </p>
+            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
+            </p>
+            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
+            </p>`
+    
+},
+     'article-two': {
+    title:'article-Two|Akanksha Dixit',
+    heading:'article-Two',
+    date:'sep 10,2016',
+    content:`<p>this is content of my second article.</p>`
+    },
+     'article-three':  {
+    title:'article-Three|Akanksha Dixit',
+    heading:'article-Three',
+    date:'sep 15,2016',
+    content:`<p>this is content of my third article.</p>`}
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+var htmlTemplate=`
+<html>
+    <head>
+        <title>${title}<?title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <div class="container">
+       <div>
+           <a href="/">Home</a>
+           
+        </div>  
+        <hr/>
+        <h3>${heading}</h3>
+        <div>
+            ${date}
+        </div>    
+        <div>
+            ${content}
+        </div>        
+ 
+          </div>
+           
+    </body>    
+    
+   </html>
+
+  `;
+  return htmlTemplate;
+}
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+    
+    var articleName = req.params.articleName; 
+  res.send(createTemplate(articles[articleName]));
+});
+
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
