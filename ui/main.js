2035c63df4e0a74679715518e6ccddeecb0b4e49
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-14 17:21:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,16 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span .innerHTML = counter.toString();
-    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readystate == XMLHttprequest.DONE){
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://akanksha2340.imad.hasura-app.io/counter',true);
+    requet.send(null);
 };
