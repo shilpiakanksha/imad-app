b71efb8761ffc5c5eb75c432648ac79f2fa764e4
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-14 14:01:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,11 +83,11 @@ app.get('/:articleName', function (req, res) {
 
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
