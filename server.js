414868d6227b29caeef116db5ffdf619b6c2b58f
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-14 16:44:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,6 +74,11 @@ var htmlTemplate=`
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString() );
+});
 
 app.get('/:articleName', function (req, res) {
     
