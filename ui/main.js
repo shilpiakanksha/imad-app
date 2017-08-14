08835e32e5090264291358a0610deb3bde30e0aa
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-14 15:38:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element = document.getElementById('main-text'
 );
 element.innerHTML = 'New value';
 
-var img = document.getElementById('img');
-img.onclick = function(){
+var img = document.getElementById('madi');
+madi.onclick = function(){
     img.style.marginLeft ='100px';
 }
