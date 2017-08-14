13dfdc5256d1cf13b875fa91a5c09bf1e8269c2d
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-14 17:32:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ button.onclick = function(){
     
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readystate == XMLHttpRequest.DONE){
+        if(request.readystate === XMLHttpRequest.DONE){
             if(request.status === 200){
                 var counter = request.responseText;
                 var span = document.getElementById('count');
@@ -13,5 +13,5 @@ button.onclick = function(){
         }
     };
     request.open('GET','http://akanksha2340.imad.hasura-app.io', true);
-    request.send();
+    request.send(null);
 };
