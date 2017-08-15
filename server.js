e9cf3bf667eaf432f4e905705b686adfd8e36a4d
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-15 12:28:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/counter',function(req,res){
     res.send(counter.toString() );
 });
 
-var names = ();
+var names = 
 app.get('/submit-name',function(req,res){
     var name = req.query.name;
     names.push(name);
