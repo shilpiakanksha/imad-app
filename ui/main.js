44d6b569ec157308d64a03e556c883e504b79606
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-15 14:07:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,122 +1,35 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-var articles={
-     'article-one': {
-    
-    title:'article-one|Akanksha Dixit',
-    heading:'article-one',
-    date:'sep 05,2016',
-    content:`<p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article
-            </p>
-            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
-            </p>
-            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
-            </p>`
-    
-},
-     'article-two': {
-    title:'article-Two|Akanksha Dixit',
-    heading:'article-Two',
-    date:'sep 10,2016',
-    content:`<p>this is content of my second article.</p>`
-    },
-     'article-three':  {
-    title:'article-Three|Akanksha Dixit',
-    heading:'article-Three',
-    date:'sep 15,2016',
-    content:`<p>this is content of my third article.</p>`}
-};
-
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-var htmlTemplate=`
+<!doctype html>
 <html>
     <head>
-        <title>${title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                   <link href="/ui/style.css" rel="stylesheet" />
+        <link href="/ui/style.css" rel="stylesheet" />
     </head>
-
     <body>
         <div class="container">
-       <div>
-           <a href="/">Home</a>
-           
-        </div>  
-        <hr/>
-        <h3>${heading}</h3>
+        <div class="center">
+            <img id="madi" src="https://pbs.twimg.com/profile_images/894970070534725632/Z-ovqzdv_400x400.jpg" class="img-medium"/>
+        </div>
+        <br>
+        <h3>personal</h3>
         <div>
-            ${date}
-        </div>    
+            Hi, my name is akanksha Dixit
+            </div>
+       <hr/>
+        <h3>profesional</h3>
         <div>
-            ${content}
-        </div>        
- 
-          </div>
-           
-    </body>    
-    
-   </html>
+           I am not working
+         </div>
+       <hr/>
+       <div class="footer">
+          this button <button id="counter"> click me</button> has been clicked <span id="count">0</span> times
+          <hr/>
+          <input type="text" id="name" placeholder="name"/>
+          <input type="submit" id="submit_btn" value="submit"/>
+          <ul id="namelist">
 
-  `;
-  return htmlTemplate;
-}
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-var counter=0;
-app.get('/counter',function(req,res){
-    counter = counter + 1;
-    res.send(counter.toString() );
-});
-
-var names = [];
-app.get('/submit-name',function(req,res){
-    var name = req.params.name;
-    
- names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
-
-
-
-
-app.get('/:articleName', function (req, res) {
-    
-    var articleName = req.params.articleName; 
-  res.send(createTemplate(articles[articleName]));
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+          </ul>
+        </div>
+        </div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
