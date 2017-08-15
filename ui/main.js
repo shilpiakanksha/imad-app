1b90752ff3d77aae568dc0f793a3022a6d2877cb
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-15 07:28:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,14 @@
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-    
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://akanksha2340.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
-submit.onclick = function(){
-    var namelist  = ['name1','name2','name3','name4','name5'];
-    var list = '';
-    for(var i=0; i< name.length; i++){
-        list+= '<li>' + name[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-    
-};
+ // conter cod
+ var button = document.getElementById('counter');
+ button.onclick = function(){
+     
+     //make a request to counter endpoint
+     
+     //capture the respone and store it in a variable
+     
+     //render variable in correct span
+     
+     counter = counter + 1;
+     var span = document. getElementById('count');
+     span.innerHTML = counter.toString();
+ };
