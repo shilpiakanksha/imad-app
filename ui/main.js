188936045eababab3e143ec25d5e94b51ce0da53
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-15 14:55:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,8 +44,32 @@ submit.onclick = function(){
     //capture a list of name and render it as list
 
     
-    
-      //make the request
+   //create a request object
+  var request = new XMLHttpRequest();
+  
+ 
+  //capture a response and store it in a variable
+  request.onreadystatechange = function(){
+     if(request.onreadyState ===  XMLHttpRequest.DONE){
+         //take some action
+         if ( request.status === 200 ){
+              var names = request.responseText;
+              names = JSON.parse(names);
+               var list = '';
+          for(var i=0; i<name.length;  i++){
+           list += '<li>' + name[i] + '</li>';
+    }
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
+             
+         }
+     }  
+     
+     //not done yet
+  };
+  
+  //make the request
+  
   request.open('GET','http://akanksha2340.imad.hasura-app.io/counter',true);
   request.send(null);
     
