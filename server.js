28b7de4c6646d919888e98e63a265adf01803c20
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-20 06:38:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('/test-db', function( req, res ){
+    //make a select request
+    //return response with result
+});
 
 var counter=0;
 app.get('/counter',function(req,res){
