1f19607498ce297b44955154afb486da0bd930d5
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-20 12:29:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/test-db', function( req, res ){
        } 
        else
        {
-       res.send(JSON.stringify(result));
+       res.send(JSON.stringify(result.rows));
        }
        
     });
