ae675971741a7fb1252eca475438f949a3b0477d
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-20 12:02:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/test-db', function( req, res ){
 });
 
 var counter=0;
-app.get('/counter',function(req,res){
+app.get('/counter',function(req, res){
     counter = counter + 1;
     res.send(counter.toString() );
 });
