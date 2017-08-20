b22ad53378847bfe947fd4921aa51771d1bb70a4
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-20 11:46:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,34 +31,41 @@ button.onclick = function(){
   request.open('GET','http://akanksha2340.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
+
 //submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //make a request to server and send name
-    //capture a list of name and render it as lis
+    
+
    //create a request object
+   
   var request = new XMLHttpRequest();
   //capture a response and store it in a variable
+  
   request.onreadystatechange = function(){
      if(request.onreadyState ===  XMLHttpRequest.DONE){
          //take some action
          if ( request.status === 200 ){
               var names = request.responseText;
+              
               names = JSON.parse(names);
                var list = '';
           for(var i=0; i< names.length;  i++){
            list += '<li>' + names[i] + '</li>';
     }
    var ul = document.getElementById('namelist');
+   
    ul.innerHTML = list;
          }
-     }  
-     //not done yet
+     } //not done yet
   };
+  
   //make the request
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
   request.open('GET','http://akanksha2340.imad.hasura-app.io/submit_name?name='+ name ,true);
- 
+ request.send(null);
 };
 
