0791cba970757fcbca3574669a3cdbd54e34bd04
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-20 12:17:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,152 +1,71 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-var Pool = require('pg').Pool;
-
-var config = {
-  user : 'akanksha2340',
-  database : 'akanksha2340',
-  host : 'db,imad.hasura-app.io',
-  port : '5432',
-  password : process.env.DB_PASSWORD
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function(){
+    
+     //create a request object
+  var request = new XMLHttpRequest();
+  //capture a response and store it in a variable
+  
+  request.readystatechange = function(){
+     if(request.onreadyState ===  XMLHttpRequest.DONE){
+         //take some action
+         
+         if ( request.status === 200 ){
+            var names = ['name1','name2','name3','name4'];
+             var list = '';
+         for(var i=0; i<name.length;  i++){
+             list += '<li>' + name[i] + '</li>';
+  }
+  
+    var counter = request.ResponseText;
+    var span = document. getElementById('count');
+    span.innerHTML(counter.toString());
+    
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
+         }
+     }  
+     //not done yet
+  };
+  //make the request
+  request.open('GET','http://akanksha2340.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
 
-var app = express();
-app.use(morgan('combined'));
 
-var articles={
-     'article-one': {
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
     
-    title:'article-one|Akanksha Dixit',
-    heading:'article-one',
-    date:'sep 05,2016',
-    content:`<p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article
-            </p>
-            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
-            </p>
-            <p>this is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first articlethis is content of my first article.
-            </p>`
-    
-},
-     'article-two': {
-    title:'article-Two|Akanksha Dixit',
-    heading:'article-Two',
-    date:'sep 10,2016',
-    content:`<p>this is content of my second article.</p>`
-    },
-     'article-three':  {
-    title:'article-Three|Akanksha Dixit',
-    heading:'article-Three',
-    date:'sep 15,2016',
-    content:`<p>this is content of my third article.</p>`}
+
+   //create a request object
+   
+  var request = new XMLHttpRequest();
+  //capture a response and store it in a variable
+  
+  request.onreadystatechange = function(){
+     if(request.readyState ===  XMLHttpRequest.DONE){
+         //take some action
+         if ( request.status === 200 ){
+              var names = request.responseText;
+              
+              names = JSON.parse(names);
+               var list = '';
+          for(var i=0; i< names.length;  i++){
+           list += '<li>' + names[i] + '</li>';
+    }
+   var ul = document.getElementById('namelist');
+   
+   ul.innerHTML = list;
+         }
+     } //not done yet
+  };
+  
+  //make the request
+
+  request.open('GET','http://akanksha2340.imad.hasura-app.io/submit_name?name='+ name ,true);
+ request.send(null);
 };
 
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-var htmlTemplate=`
-<html>
-    <head>
-        <title>${title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                   <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-
-    <body>
-        <div class="container">
-       <div>
-           <a href="/">Home</a>
-           
-        </div>  
-        <hr/>
-        <h3>${heading}</h3>
-        <div>
-            ${date}
-        </div>    
-        <div>
-            ${content}
-        </div>        
- 
-          </div>
-           
-    </body>    
-    
-   </html>
-
-  `;
-  return htmlTemplate;
-}
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-
-
-
-var Pool = new Pool(config);
-app.get('/test-db', function( req, res ){
-    //make a select request
-    //return response with result
-    pool.query('SELECT * from test', function(req, res){
-       if(err){
-           res.status(500).send(err.toString());
-       } 
-       else
-       {
-       res.send(JSON.stringify(result));
-       }
-       
-    });
-    
-});
-
-var counter=0;
-app.get('/counter',function(req, res){
-    counter = counter + 1;
-    res.send(counter.toString() );
-});
-
-var names = [];
-app.get('/submit-name',function(req,res){
-    var name = req.query.name;
-    
- names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
-
-
-
-
-app.get('/:articleName', function (req, res) {
-    
-    var articleName = req.params.articleName; 
-  res.send(createTemplate(articles[articleName]));
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
