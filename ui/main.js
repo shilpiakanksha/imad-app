6cca2533889ad80ce715bf0a86c1af4b308a3649
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-20 12:17:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,22 +8,16 @@ button.onclick = function(){
   
   request.onreadystatechange = function(){
      if(request.readyState ===  XMLHttpRequest.DONE){
-         //take some action
-         
-         if ( request.status === 200 ){
-            var names = ['name1','name2','name3','name4'];
-             var list = '';
-         for(var i=0; i<name.length;  i++){
-             list += '<li>' + name[i] + '</li>';
-  }
-  
-    var counter = request.ResponseText;
+           var counter = request.responseText;
     var span = document. getElementById('count');
     span.innerHTML(counter.toString());
+  }
+  
+
     
    var ul = document.getElementById('namelist');
    ul.innerHTML = list;
-         }
+         };
      }  
      //not done yet
   };
