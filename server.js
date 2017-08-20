c7737f2876bfe8f1687b93e3e470fa3701858ad2
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-20 06:47:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+  user : 'akanksha2340',
+  database : 'akanksha2340',
+  host : 'db.imad.hasura-app.io',
+  port : '5432',
+  password : process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
