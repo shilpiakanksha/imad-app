324a4c668776025d2b2ece3b5d7c0a561017bd25
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-21 15:52:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.get('/submit-name',function(req,res){
     
 });
 
-app.get('/articles/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) {
     
     //SELECT * FROM article where title = ''; DELETE where a = asdf
     Pool.query("SELECT * FROM article  WHERE title ='" + req.params.articleName + "'" ,function(err, result){
