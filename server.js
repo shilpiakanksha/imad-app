d4d296485000635108fe825cd3bff9fd45e160e6
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-08-24 20:27:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.post('/login',  function(req, res){
     var password = req.body.password;
     
   
-    Pool.query('SELECT * FROM "user" WHERE username = $1'  ,[username], function(err, result){
+    pool.query('SELECT * FROM "user" WHERE username = $1'  ,[username], function(err, result){
          if(err){
            res.status(500).send(err.toString());
        } 
