3127dca5db1171431f77adb32b56a4d735f5dbd8
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-09-06 16:58:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.post('/login',  function(req, res){
     
 });
 
-var Pool = new pool(config);
+var pool = new Pool(config);
 app.get('/test-db', function( req, res ){
     //make a select request
     //return response with result
