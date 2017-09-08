667d48e9b8fc048fa900fa8a246d5267de894ad6
shilpiakanksha <30656886+shilpiakanksha@users.noreply.github.com>
2017-09-08 21:06:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.post('/login',  function(req, res){
 });
 
 app.get('/check-login',function(req, res){
-    if(req.session && req.session.auth && request.session.auth.userId){
+    if(req.session && req.session.auth && req.session.auth.userId){
         res.send('you are logged in:' + req.session.auth.userId.toString());
     }else
     {
